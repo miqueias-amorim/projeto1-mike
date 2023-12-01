5917ddcf249b0a2a85651edd020930c5baa018b5
Miquéias Amorim <miqueias.aandrade@gmail.com>
2023-12-01 10:56:28 -0300

    commit inicial

---
+++ mediaaritmeticaa.py
@@ -0,0 +1,6 @@
+primerianota = int(input("Digite a primeira nota: "))
+segundanota = int(input("Digite a segunda nota: "))
+terceiranota = int(input("Digite a terceira nota: "))
+quartanota = int(input("Digite a quarta nota: "))
+mediaaritmetica = primerianota + segundanota + terceiranota + quartanota // 4
+print("A média aritmética é " mediaaritmetica)
