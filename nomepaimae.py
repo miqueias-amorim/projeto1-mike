99981cbaaf382190ac08457b844dd9b9684e583c
Miquéias Amorim <miqueias.aandrade@gmail.com>
2023-12-01 10:14:19 -0300

    Versao 2

--- nomepaimae.py
+++ nomepaimae.py
@@ -1,5 +1,7 @@
 nomeDaMae = input("Qual o nome da sua mãe? ")
 nomeDoPai = input("Qual o nome do seu pai? ")
 
-print("O nome da sua mãe é ", nomeDaMae)
-print("O nome do seu pai é ", nomeDoPai)
+print("Bom dia Sra. ", nomeDaMae, "!! E bom dia Sr.", nomeDoPai, "!!")
+
+
+
