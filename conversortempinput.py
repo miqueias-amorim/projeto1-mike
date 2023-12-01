85a71247953d433474f7029f0df9d6e5e4560d79
Miquéias Amorim <miqueias.aandrade@gmail.com>
2023-12-01 10:00:43 -0300

    commit inicial

---
+++ conversortempinput.py
@@ -0,0 +1,7 @@
+temperaturaFahrenheint = input("Digite uma temperatura em Fahrenheint ")
+
+temp = float(temperaturaFahrenheint)
+
+temperaturaCelsius = (temp - 32) * 5 / 9
+
+print ("A temperatura em graus celsius é", temperaturaCelsius)
