4d97cc1d01beb78e39f64f7d776e59a32b448e51
Miquéias Amorim <miqueias.aandrade@gmail.com>
2023-12-01 15:59:54 -0300

    commit inicial

---
+++ faturacartãomsg.py
@@ -0,0 +1,11 @@
+cliente = input("Digite o nome do cliente: ")
+
+diavencimento = input("Digite o dia de vencimento: ")
+
+mesvencimento = input("Digite o mês de vencimento: ")
+
+valorfatura = input("Digite o valor da fatura: ")
+
+print("Olá, ", cliente)
+
+print("A sua fatura com vencimento em ", diavencimento, "de ", mesvencimento, "no valor de ", valorfatura, "está fechada.")
